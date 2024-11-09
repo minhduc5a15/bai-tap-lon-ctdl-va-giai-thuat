610a768e5c3264f272b0b5d64cefc6d0d90f3ad4
minhduc5a15 <minhduc5a15@gmail.com>
2024-11-09 21:28:25 +0700

    thử test cases

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@ void run_A() {
 }
 
 void run_B() {
-	const calc_expression expression("123 + 54 * (17 + 65 * (65 - 12))");
+	// Trường hợp dấu - đứng đầu
+	calc_expression expression("-123 + 72313 * (-123)");
+	cout << "postfix expression = " << expression.post_fix() << endl;
+	cout << expression.get_expression() << " = " << expression.calculate() << endl;
+	// Trường hợp dấu + đứng đầu
+	expression = calc_expression("-123 + 72313 * (+123)");
+	cout << "postfix expression = " << expression.post_fix() << endl;
+	cout << expression.get_expression() << " = " << expression.calculate() << endl;
+	// Trường hợp bình thường
+	expression = calc_expression("1+2+3+4+5+6+7+8+9+10*(11*(12-13)+13*(14+15))/5");
 	cout << "postfix expression = " << expression.post_fix() << endl;
 	cout << expression.get_expression() << " = " << expression.calculate() << endl;
 }
